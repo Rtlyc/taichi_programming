3e84feed81163830327b320ca82047720f16c1c8
Rtlyc <yuchenliu0513@gmail.com>
2021-10-28 11:04:27 +0800

    not workable return completed version

--- Ray Tracing/scripts/test.py
+++ Ray Tracing/scripts/test.py
@@ -1,16 +1,22 @@
+from math import pi
 import taichi as ti
 import numpy as np
-import time 
+import sys
 from ray import Rays
 from hittable import *
+from camera import *
 
 ####### Initialization ########
 ti.init(ti.cpu)
 aspect_ratio = 16.0/9.0
 WIDTH = 400
 HEIGHT = int(400/aspect_ratio)
-pixels = ti.Vector.field(3, dtype=ti.u8)   
-ti.root.dense(ti.ij, (WIDTH, HEIGHT)).place(pixels) 
+samples_per_pixel = 100
+pixels = ti.Vector.field(3, dtype=ti.f32)   
+color_pixels = ti.Vector.field(3, dtype=ti.u8)
+sample_count = ti.field(dtype=ti.i32)
+needs_sample = ti.field(dtype=ti.i32)
+ti.root.dense(ti.ij, (WIDTH, HEIGHT)).place(pixels, sample_count, needs_sample, color_pixels) 
 
 rays = Rays(WIDTH,HEIGHT)
 
@@ -24,6 +30,7 @@ world.finalize()
 viewport_height = 2.0
 viewport_width = aspect_ratio * viewport_height
 focal_length = 1.0
+cam = Camera()
 
 origin = ti.Vector([0.0,0.0,0.0])
 horizontal = ti.Vector([viewport_width,0,0])
@@ -31,6 +38,18 @@ vertical = ti.Vector([0,viewport_height,0])
 lower_left_corner = origin-horizontal/2-vertical/2-ti.Vector([0,0,focal_length])
 
 ###### Operation #########
+import random 
+@ti.func
+def random_in_unit_sphere(small, large):
+    cur = ti.Vector([1.0,1.0,1.0])
+    while True:
+        cur = ti.Vector([random.uniform(small, large), random.uniform(small, large), random.uniform(small, large)])
+        if cur.norm_sqr()<1.0:
+            break 
+    return cur 
+            
+
+
 @ti.func
 def hit_sphere(center, radius, origin, direction):
     oc = origin - center
@@ -45,55 +64,69 @@ def hit_sphere(center, radius, origin, direction):
     return result 
 
 @ti.func
-def ray_color(x,y,world):
-    ray_origin = rays.origins[x,y]
-    ray_direction = rays.directions[x,y]
+def ray_color(ray_origin, ray_direction, world):
     result = ti.Vector([1.0,0.0,0.0])
 
-    # temp_record = HitRecord(0.0)
-    hit_anything, normal = world.hit(ray_origin, ray_direction, 0, INFINITY)
+    hit_anything, normal, p = world.hit(ray_origin, ray_direction, 0, INFINITY)
 
-    # t = hit_sphere(ti.Vector([0.0,0.0,-1.0]),0.5,origin, direction)
     if hit_anything:
-        # n = rays.get_at(x,y,t) - ti.Vector([0.0,0.0,-1.0])
-        # n = n.normalized()
+        target = p+normal+random_in_unit_sphere(-1.0,1.0) 
+        # rays.origins[x,y] = p 
+        # rays.origins[x,y] = target-p
         result = 0.5*(normal+ti.Vector([1.0,1.0,1.0])) 
-        # result = 0.5*ti.Vector([n.x+1.0,n.y+1.0,n.z+1.0])*255.999
+        # result = 0.5*()
     else:
         unit_direction = ray_direction.normalized()
-        # print(unit_direction)
         t = 0.5 * (unit_direction.y+1.0)
         result = ((1.0-t)*ti.Vector([1.0,1.0,1.0])+t*ti.Vector([0.5,0.7,1.0]))
-    return result*255.999
+    return result
 
 
 @ti.kernel  
 def initialize():
-    for x,y in pixels:
-        u = x / (WIDTH-1)
-        v = y / (HEIGHT-1)
+    for x,y in sample_count:
+        sample_count[x,y] = 0
+        needs_sample[x,y] = 1
 
-        rays.origins[x,y] = origin
-        rays.directions[x,y] = lower_left_corner+u*horizontal + v*vertical
-        pixels[x,y] = ray_color(x,y,world)
-        # print("remaining:", WIDTH-x)
+@ti.kernel  
+def render() -> ti.i32:
+    completed = 0
+    for x,y in pixels:
+        if sample_count[x,y] == samples_per_pixel: 
+            continue
+        u = (x+random.random()) / (WIDTH-1)
+        v = (y+random.random()) / (HEIGHT-1)
+        origin,direction = cam.get_ray(u,v)
+
+        # rays.origins[x,y] = origin
+        # rays.directions[x,y] = direction
+        pixels[x,y] += ray_color(origin,direction,world)/samples_per_pixel
+        # pixels[x,y] += ti.Vector([120,20,50])
+        sample_count[x,y] += 1
+        if sample_count[x,y] == samples_per_pixel:
+            completed += 1
+    return completed
+
+@ti.kernel
+def finalize():
+    for x,y in pixels:
+        pixels[x,y] = pixels[x,y]/samples_per_pixel
 
 
 
 ###### Rendering #########
 initialize()
-# last_t = 0
-# for i in range(50000):
-#     # render()
-#     interval = 10
-#     if i % interval == 0 and i > 0:
-#         print("{:.2f} samples/s".format(interval / (time.time() - last_t)))
-#         last_t = time.time()
-#         img = pixels.to_numpy() * (1 / (i + 1))
-#         img = img / img.mean() * 0.24
-#         # gui.set_image(np.sqrt(img))
-#         # gui.show()
+pixels_completed = 0
+pixels_all = WIDTH*HEIGHT
+while pixels_completed<pixels_all:
+    complete = render()
+    # print(complete)
+    pixels_completed += complete
+    # print(pixels_completed/pixels_all, file=sys.stderr)
+# finalize()
 
 ti.imwrite(pixels.to_numpy(), 'out.png')
-print(pixels.to_numpy())
+# print(pixels.to_numpy())
+print(HEIGHT)
+# print(color_pixels.to_numpy())
 
